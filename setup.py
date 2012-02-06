05745e7aff57f4bbcdb47085dd3a4df7a7c9298e
Andrew Cutler <macropin@gmail.com>
2012-02-06 16:44:08 +1100

    Minimal setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-adlibre',
+      version='1.0',
+      description='Miscellaneous Django apps and helpers',
+      author='Adlibre Pty Ltd',
+      author_email='code@adlibre.com.au',
+      url='https://github.com/adlibre/django-adlibre',
+     )
